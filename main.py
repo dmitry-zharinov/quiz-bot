10a70e8479a73304df17fe0235250892ecded1bc
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-11-16 08:57:42 +0300

    Parse comments and improve quiz parsing

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
 from random import randint
+from itertools import count
 
 
 def parse_quiz_from_file(filepath):
     quiz = {}
+    question = ''
+    answer = ''
+    comment = ''
+
+    counter = count()
+    number = next(counter)
+
     with open(filepath, "r", encoding="KOI8-R") as file:    
         for text in file.read().split("\n\n"):
             if not text:
                 continue
-            number = randint(0, 1_000_000)
             header, body = text.split("\n", maxsplit=1)
-            if header.startswith("Вопрос"):
-                quiz[f"Вопрос {number}"] = " ".join(body.split())
-            if header.startswith("Ответ"):
-                quiz[f"Ответ {number}"] = " ".join(body.split())
+            if header.lower().strip().startswith("вопрос"):
+                question = " ".join(body.split())
+
+            if header.lower().strip().startswith("ответ"):
+                answer = " ".join(body.split())
+
+            if header.lower().strip().startswith("комментарий"):
+                comment = " ".join(body.split())
+
+            if question and answer:
+                quiz[number] = {
+                    "question": question,
+                    "answer": answer,
+                    "comment": comment,
+
+                }
+
+                number = next(counter)
+                question = ''
+                answer = ''
+                comment = ''
     return quiz
 
 
 def main():
-    with open("quiz-questions/120br.txt", "r", encoding="KOI8-R") as file:
-        quiz = parse_quiz_from_file(file)
-    for q, a in quiz.items():
-        print(q)
-        print(a)
+    quiz = parse_quiz_from_file('quiz-questions/120br.txt')
+    for key, val in quiz.items():
+        print(f'key: {key}')
+        print(f'val: {val}')
 
 
 if __name__ == "__main__":
